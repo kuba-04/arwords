997b35ae881c98b3152ef951ce2636af291156c9
Jakub Gladysz <jakub.gladysz@protonmail.com>
2025-06-23 16:30:10 +0300

    ndkversion update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.arwords"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973" //temporarily
+    ndkVersion = "25.1.8937393"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
