fe986056ef04ceed9ceb1235a13221bcc731bba1
Jakub Gladysz <jakub.gladysz@protonmail.com>
2025-08-29 11:38:36 +0200

    images and banner

--- lib/main.dart
+++ lib/main.dart
@@ -79,6 +79,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Arabic Words',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
